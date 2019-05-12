b0f2b1301087533fabf5ee8b9e19909d009c4d18
DanilaMor <32496790+DanilaMor@users.noreply.github.com>
2019-05-12 14:28:56 +0300

    add calculate the number of decimal places

--- submit.js
+++ submit.js
@@ -2,9 +2,25 @@ function summa(){
     var A = document.forms["form"].elements["A"].value;
     var B = document.forms["form"].elements["B"].value;
 
+    // calculate the number of decimal places
+    var text;
+    var numberOfSimbols = 0;
+    if (A.indexOf(".") != -1){
+        text = A.split(".")[1];
+        numberOfSimbols = text.length
+    }
+
+    if (B.indexOf(".") != -1){
+        text = A.split(".")[1];
+        if (numberOfSimbols < text.length){
+            numberOfSimbols = text.length
+        }
+    }
+
     A = parseFloat(A);
     B = parseFloat(B);
 
+
     var summaAB = A + B;
-    alert("Результат: \n " + summaAB);
+    alert("Результат: \n " + summaAB.toFixed(numberOfSimbols));
 }
